6b9fccbd3c5afa9ffcb98847b6d088a2fc2fbe4a
Connor Clarke <connorclarke1@yahoo.com>
2024-04-29 14:52:07 +0100

    Script new executable from cmd with py [scriptLocation] [imageFileLocation]

--- BarcodeScan.py
+++ BarcodeScan.py
@@ -1,12 +1,27 @@
 import cv2, zxingcpp
+import sys
 
-img = cv2.imread('testBarcode.jpeg')
-results = zxingcpp.read_barcodes(img)
-for result in results:
-	print('Found barcode:'
-		f'\n Text:    "{result.text}"'
-		f'\n Format:   {result.format}'
-		f'\n Content:  {result.content_type}'
-		f'\n Position: {result.position}')
-if len(results) == 0:
-	print("Could not find any barcode.")
+##	#	f'\n Text:    "{result.text}"'
+##	#	f'\n Format:   {result.format}'
+##	#	f'\n Content:  {result.content_type}'
+##	#	f'\n Position: {result.position}')
+
+
+if __name__ == "__main__":
+    try:
+        imgPath = str(sys.argv[1])
+        img = cv2.imread(imgPath)
+        results = zxingcpp.read_barcodes(img)
+        if len(results) == 0:
+            print("Error")
+            print("No Barcode Detected")
+        elif len(results) > 1:
+            print("Error")
+            print("Multiple barcodes detected")
+        else:
+            print("Success")
+            print(results[0].format)
+            print(results[0].text) 
+    except:
+        print("Error")
+        print("Serverside")
